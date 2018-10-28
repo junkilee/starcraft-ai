7c3cc214bf7c7ccd7f300251df138da47a80b14f
Brendan Le <le.brendan@gmail.com>
2018-10-27 20:27:57 -0400

    Improve a3c

--- sc2ai/actor_critic.py
+++ sc2ai/actor_critic.py
@@ -9,11 +9,12 @@ class ActorCritic(ABC):
         self.state_shape = state_shape
 
     @abstractmethod
-    def actor(self, states):
+    def actor(self, states, action_mask):
         """
         Tensorflow operation that maps states to actions
-        :param states: A tensor of shape [num_instances, num_steps, *state_shape]
-        :return: A tensor of shape [num_instances, num_steps, num_actions]
+        :param states: A tensor of shape [num_steps, *state_shape]
+        :param action_mask: A tensor of shape [num_steps, num_actions]
+        :return: A tensor of shape [num_steps, num_actions]
         """
         pass
 
@@ -21,8 +22,8 @@ class ActorCritic(ABC):
     def critic(self, states):
         """
         Tensorflow operation that maps states to values
-        :param states: A tensor of shape [num_instances, num_steps, *state_shape]
-        :return: A tensor of shape [num_instances, num_steps]
+        :param states: A tensor of shape [num_steps, *state_shape]
+        :return: A tensor of shape [num_steps, num_steps]
         """
         pass
 
@@ -37,19 +38,20 @@ class BasicActorCritic(ActorCritic):
         self.dropout_prob = dropout_prob
 
     def shared_layers(self, state):
-        state = tf.reshape(state, [1, np.prod(self.state_shape)])
+        state = tf.reshape(state, [-1, np.prod(self.state_shape)])
         with tf.variable_scope('shared', reuse=tf.AUTO_REUSE):
             for i, layer_size in enumerate(self.shared_architecture):
                 state = tf.layers.dense(state, layer_size, activation=tf.nn.tanh, name='shared%d' % i)
         return state
 
-    def actor(self, state):
+    def actor(self, state, action_mask):
         state = self.shared_layers(state)
         with tf.variable_scope('actor', reuse=tf.AUTO_REUSE):
             for i, layer_size in enumerate(self.architecture):
                 state = tf.layers.dense(state, layer_size, activation=tf.nn.tanh, name='layer%d' % i)
             probs = tf.layers.dense(state, units=self.num_actions, activation=tf.nn.softmax, name='output')
-        return tf.squeeze(probs)
+        masked = probs * action_mask
+        return masked / tf.reduce_sum(masked, axis=1, keepdims=True)
 
     def critic(self, state):
         state = self.shared_layers(state)
@@ -58,3 +60,41 @@ class BasicActorCritic(ActorCritic):
                 state = tf.layers.dense(state, layer_size, activation=tf.nn.tanh, name='critic%d' % i)
             state = tf.layers.dense(state, 1, activation=None, name='output')
         return tf.squeeze(state, axis=-1)
+
+
+class ConvActorCritic(ActorCritic):
+    def __init__(self, num_actions, state_shape, dropout_prob=0):
+        super().__init__(num_actions, state_shape)
+        self.reward_architecture = (32,)
+        self.dropout_prob = dropout_prob
+
+        self.filters = [16, 16, 16, 16]
+        self.kernel_sizes = [7, 5, 3, 3]
+        self.strides = [3, 2, 1, 1]
+        self.architecture = list(zip(range(len(self.filters)), self.filters, self.kernel_sizes, self.strides))
+
+    def shared_layers(self, state):
+        logits = tf.transpose(state, [0, 3, 2, 1])  # Transpose into channels last format
+        with tf.variable_scope('ac_shared', reuse=tf.AUTO_REUSE):
+            for i, filters, kernel_size, strides in self.architecture:
+                logits = tf.layers.conv2d(logits, filters, kernel_size, strides, activation=tf.nn.leaky_relu,
+                                          kernel_initializer=tf.truncated_normal_initializer(stddev=0.01),
+                                          name='conv%d' % i)
+            logits = tf.layers.flatten(logits)
+            logits = tf.layers.dense(logits, units=64, activation=tf.nn.leaky_relu, name='d1')
+
+        logits = tf.reshape(logits, [-1, 64])
+        return logits
+
+    def actor(self, state, action_mask):
+        features = self.shared_layers(state)
+        with tf.variable_scope('actor', reuse=tf.AUTO_REUSE):
+            probs = tf.layers.dense(features, units=self.num_actions, activation=tf.nn.softmax, name='output')
+        masked = probs * action_mask
+        return masked / tf.reduce_sum(masked, axis=1, keepdims=True)
+
+    def critic(self, state):
+        features = self.shared_layers(state)
+        with tf.variable_scope('critic', reuse=tf.AUTO_REUSE):
+            features = tf.layers.dense(features, units=1, activation=None, name='output')
+        return tf.squeeze(features, axis=-1)
--- sc2ai/learned_agents.py
+++ sc2ai/learned_agents.py
@@ -1,36 +1,58 @@
 import numpy as np
 from pysc2.agents import base_agent
 from pysc2.lib import actions
-from pysc2.lib import units
-from pysc2.lib import features
+from pysc2.env.environment import StepType
 
-from sc2ai.actor_critic import BasicActorCritic
+from sc2ai.actor_critic import ConvActorCritic
 import tensorflow as tf
 
 
 class RoachesAgent(base_agent.BaseAgent):
     def __init__(self):
         super().__init__()
+        self.discount_factor = 0.99
         state_shape = [7, 64, 64]
-        self.actions = [
+        self.action_options = [
             actions.FUNCTIONS.Attack_screen('now', [0, 0]),
             actions.FUNCTIONS.Attack_screen('now', [83, 0]),
             actions.FUNCTIONS.Attack_screen('now', [0, 83]),
             actions.FUNCTIONS.Attack_screen('now', [83, 83]),
             actions.FUNCTIONS.select_army('select')
         ]
-        self.actor_critic = BasicActorCritic(num_actions=len(self.actions),
-                                             state_shape=state_shape, architecture=(16,))
-        self.state_input = tf.placeholder(tf.float32, state_shape, name='state_input')
-        self.actor_probs = self.actor_critic.actor(self.state_input)
+        self.num_actions = len(self.action_options)
+        self.actor_critic = ConvActorCritic(num_actions=self.num_actions, state_shape=state_shape)
+        self.state_input = tf.placeholder(tf.float32, [None, *state_shape], name='state_input')
+        self.action_mask_input = tf.placeholder(tf.float32, [None, self.num_actions], name='available_actions')
+
+        self.state_value = self.actor_critic.critic(self.state_input)
+        self.actor_probs = self.actor_critic.actor(self.state_input, self.action_mask_input)
+
+        self.discounted_reward_input = tf.placeholder(tf.float32, [None], name='discounted_rewards')
+        self.actions_input = tf.placeholder(tf.int32, [None], name='actions')
+        actions_one_hot = tf.one_hot(self.actions_input, self.num_actions)
+        action_probs = tf.reduce_sum(self.actor_probs * actions_one_hot, axis=-1)
+        advantage = self.discounted_reward_input - self.state_value
+
+        self.entropy_bonus = tf.reduce_sum(0.1 * self.entropy(self.actor_probs))
+        self.critic_loss = tf.reduce_sum(tf.square(advantage))
+        self.actor_loss = -tf.reduce_sum(tf.log(action_probs) * tf.stop_gradient(advantage))
+        self.loss = self.actor_loss - self.entropy_bonus + self.critic_loss
+        self.train_op = tf.train.AdamOptimizer(0.001).minimize(self.loss)
+
         self.session = tf.Session()
         self.session.run(tf.global_variables_initializer())
 
+        self.states, self.actions, self.rewards, self.action_masks = [], [], [], []
+
     @staticmethod
-    def remove_unavailable_actions(observation, action_probs):
-        if actions.FUNCTIONS.Attack_screen.id not in observation.available_actions:
-            action_probs -= [1, 1, 1, 1, 0]
-        return action_probs
+    def entropy(probs):
+        return -tf.reduce_sum(probs * tf.log(probs + 1e-10), axis=-1)
+
+    def get_action_mask(self, available_actions):
+        mask = np.ones([self.num_actions])
+        if actions.FUNCTIONS.Attack_screen.id not in available_actions:
+            mask[:4] = 0
+        return mask
 
     def step(self, obs):
         """
@@ -38,19 +60,60 @@ class RoachesAgent(base_agent.BaseAgent):
         :return: states, reward, done
         """
         super().step(obs)
+        state = obs.observation.feature_minimap.astype(np.float32)
 
-        action_probs = self.session.run(self.actor_probs, feed_dict={
-            self.state_input: obs.observation.feature_minimap.astype(np.float32)
+        if obs.step_type != StepType.FIRST:
+            self.rewards.append(obs.reward)
+
+        if obs.step_type != StepType.LAST:
+            self.states.append(state)
+            action_mask = self.get_action_mask(obs.observation.available_actions)
+            action_probs = self.session.run(self.actor_probs, feed_dict={
+                self.state_input: np.expand_dims(state, axis=0),
+                self.action_mask_input: action_mask[np.newaxis]
+            })
+            chosen_action_index = np.random.choice(self.num_actions, p=action_probs[0])
+            self.actions.append(chosen_action_index)
+            self.action_masks.append(action_mask)
+            return self.action_options[chosen_action_index]
+
+    def discount(self, rewards):
+        prev = 0
+        discounted_rewards = np.copy(rewards)
+        for i in range(1, len(discounted_rewards)):
+            discounted_rewards[-i] += prev * self.discount_factor
+            prev = discounted_rewards[-i]
+        return discounted_rewards
+
+    def train_policy(self):
+        discounted_rewards = self.discount(self.rewards)
+        loss, _ = self.session.run([self.loss, self.train_op], feed_dict={
+            self.state_input: np.array(self.states),
+            self.discounted_reward_input: discounted_rewards,
+            self.actions_input: self.actions,
+            self.action_mask_input: np.stack(self.action_masks)
         })
-        # chosen_action_index = np.random.choice(len(self.actions), None, False, action_probs)
-        chosen_action_index = np.random.choice(self.remove_unavailable_actions(obs.observation, action_probs))
-        return self.actions[chosen_action_index]
+        print("Total reward: %.3f" % np.sum(self.rewards))
+        print("Loss: %.3f" % loss)
 
     def reset(self):
+        """
+        Gets called after each episode. Training occurs here.
+        """
+        if len(self.states) != 0:
+            self.train_policy()
+        self.states, self.actions, self.rewards, self.action_masks = [], [], [], []
         super().reset()
 
     def setup(self, obs_spec, action_spec):
         super().setup(obs_spec, action_spec)
+
+    @staticmethod
+    def _xy_locs(mask):
+        """Mask should be a set of bools from comparison with a feature layer."""
+        y, x = mask.nonzero()
+        return list(zip(x, y))
+
 
 
 
