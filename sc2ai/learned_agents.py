1301d72d798503b599d7a8b20e0d5dc5479ea4de
Brendan Le <le.brendan@gmail.com>
2018-11-03 17:28:37 -0400

    Add conv actor critic

--- sc2ai/learned_agents.py
+++ sc2ai/learned_agents.py
@@ -6,6 +6,7 @@ from pysc2.env.environment import StepType
 import torch
 import torch.nn.functional as F
 from torch.distributions import Categorical
+from pysc2.lib import features
 
 
 class SimpleActor(torch.nn.Module):
@@ -17,12 +18,20 @@ class SimpleActor(torch.nn.Module):
         self.linear1 = torch.nn.Linear(np.prod(self.state_shape), 32)
         self.linear2 = torch.nn.Linear(32, self.num_actions)
 
-    def forward(self, state):
+    def forward(self, state, action_mask):
         logits = state.view(-1, np.prod(self.state_shape))
         logits = self.linear1(logits)
         logits = F.relu(logits)
         logits = self.linear2(logits)
-        return F.softmax(logits, dim=-1)
+
+        action_probs = F.softmax(logits, dim=-1)
+
+        masked_probs = action_probs * action_mask.type(torch.FloatTensor)
+        masked_probs = masked_probs / masked_probs.sum()
+        categorical = Categorical(masked_probs)
+        chosen_action_index = categorical.sample()
+        log_action_prob = categorical.log_prob(chosen_action_index)
+        return chosen_action_index, log_action_prob
 
 
 class SimpleCritic(torch.nn.Module):
@@ -40,18 +49,77 @@ class SimpleCritic(torch.nn.Module):
         return self.linear2(logits)
 
 
+class ConvActor(torch.nn.Module):
+    def __init__(self, num_actions, state_shape):
+        super().__init__()
+        self.num_actions = num_actions
+        self.state_shape = state_shape
+        num_input_channels = state_shape[0]
+
+        self.conv1 = torch.nn.Conv2d(num_input_channels, 32, 5, stride=3)
+        self.conv2 = torch.nn.Conv2d(32, 32, 5, stride=3)
+        self.conv3 = torch.nn.Conv2d(32, 16, 3)
+
+        self.linear1 = torch.nn.Linear(576, 32)
+        self.linear2 = torch.nn.Linear(32, self.num_actions)
+
+    def forward(self, state, action_mask):
+        num_steps = state.shape[0]
+
+        logits = F.relu(self.conv1(state))
+        logits = F.relu(self.conv2(logits))
+        logits = F.relu(self.conv3(logits))
+        logits = logits.view(num_steps, -1)
+        logits = F.relu(self.linear1(logits))
+        logits = self.linear2(logits)
+        action_probs = F.softmax(logits, dim=-1)
+
+        masked_probs = action_probs * action_mask.type(torch.FloatTensor)
+        masked_probs = masked_probs / masked_probs.sum()
+        categorical = Categorical(masked_probs)
+        chosen_action_index = categorical.sample()
+        log_action_prob = categorical.log_prob(chosen_action_index)
+        return chosen_action_index, log_action_prob
+
+
+class ConvCritic(torch.nn.Module):
+    def __init__(self, state_shape):
+        super().__init__()
+        self.state_shape = state_shape
+        num_input_channels = state_shape[0]
+
+        self.conv1 = torch.nn.Conv2d(num_input_channels, 32, 5, stride=3)
+        self.conv2 = torch.nn.Conv2d(32, 32, 5, stride=3)
+        self.conv3 = torch.nn.Conv2d(32, 16, 3)
+
+        self.linear1 = torch.nn.Linear(576, 32)
+        self.linear2 = torch.nn.Linear(32, 1)
+
+    def forward(self, state):
+        num_steps = state.shape[0]
+
+        logits = F.relu(self.conv1(state))
+        logits = F.relu(self.conv2(logits))
+        logits = F.relu(self.conv3(logits))
+        logits = logits.view(num_steps, -1)
+        logits = F.relu(self.linear1(logits))
+        logits = self.linear2(logits)
+        return logits
+
+
 class RoachesAgent(base_agent.BaseAgent):
     def __init__(self):
         super().__init__()
         self.discount_factor = 0.99
 
         # This is the state shape for the mini-map, represented in channels_first order.
-        state_shape = [7, 64, 64]
+        state_shape = [2, 84, 84]
 
+        num_grid_points = (8, 6)  # Number of points going horizontally, vertically
         points = []
-        for i in range(4):
-            for j in range(4):
-                points.append((i * int(83 / 3), j * int(83 / 3)))
+        for i in range(num_grid_points[0]):
+            for j in range(num_grid_points[1]):
+                points.append((i * int(83 / (num_grid_points[0] - 1)), j * int(63 / (num_grid_points[1] - 1))))
 
         # Available moves for agent include attack-moving into the corner.
         self.action_options = [
@@ -63,9 +131,9 @@ class RoachesAgent(base_agent.BaseAgent):
 
         self.num_actions = len(self.action_options)
 
-        self.actor = SimpleActor(num_actions=self.num_actions, state_shape=state_shape)
-        self.critic = SimpleCritic(state_shape=state_shape)
-        self.optimizer = torch.optim.Adam(list(self.actor.parameters()) + list(self.critic.parameters()), lr=0.001)
+        self.actor = ConvActor(num_actions=self.num_actions, state_shape=state_shape)
+        self.critic = ConvCritic(state_shape=state_shape)
+        self.optimizer = torch.optim.Adam(list(self.actor.parameters()) + list(self.critic.parameters()), lr=0.0005)
 
         # Define all input placeholders
         self.states, self.rewards, self.log_action_probs = [], [], []
@@ -91,7 +159,12 @@ class RoachesAgent(base_agent.BaseAgent):
         :return: states, reward, done
         """
         super().step(obs)
-        state = obs.observation.feature_minimap.astype(np.float32)
+
+        player_relative = obs.observation.feature_screen.player_relative
+        beacon = (np.array(player_relative) == 3).astype(np.float32)
+        player = (np.array(player_relative) == 1).astype(np.float32)
+
+        state = np.stack([beacon, player], axis=0)
 
         if obs.step_type != StepType.FIRST:
             self.rewards.append(obs.reward)
@@ -99,15 +172,8 @@ class RoachesAgent(base_agent.BaseAgent):
         if obs.step_type != StepType.LAST:
             self.states.append(state)
             action_mask = self.get_action_mask(obs.observation.available_actions)
-            action_probs = self.actor(torch.as_tensor(np.expand_dims(state, axis=0)))
-
-            masked_probs = action_probs * action_mask.type(torch.FloatTensor)
-            masked_probs = masked_probs / masked_probs.sum()
-            categorical = Categorical(masked_probs)
-            chosen_action_index = categorical.sample()
-            log_action_prob = categorical.log_prob(chosen_action_index)
-
-            # chosen_action_index = np.random.choice(self.num_actions, p=action_probs[0])
+            chosen_action_index, log_action_prob = self.actor(
+                torch.as_tensor(np.expand_dims(state, axis=0)), action_mask.type(torch.FloatTensor))
 
             self.log_action_probs.append(log_action_prob)
             return self.action_options[chosen_action_index]
