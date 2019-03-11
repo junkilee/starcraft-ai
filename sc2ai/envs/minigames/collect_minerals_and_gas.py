c6ee11b50e9baa2da7b7cbd02ff04653691b3733
Jun Ki Lee <joongee@gmail.com>
2019-03-11 02:31:43 -0400

    adding minigame files

---
+++ sc2ai/envs/minigames/collect_minerals_and_gas.py
@@ -0,0 +1,16 @@
+import numpy as np
+from gym import spaces
+from pysc2.lib import actions, features
+
+from minigame import MiniGameEnv
+
+class DefeatZerglingsEnv(MiniGameEnv):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+    
+    def _process_reward(self, reward, raw_obs):
+        raise NotImplementedError
+
+    def _process_observation(self, raw_obs):
+        raise NotImplementedError
+
