Fix attention bug where it doesn't work on arbitrary number of units

sc2ai/env_interface.py
@@ -87,26 +87,25 @@ class EmbeddingInterfaceWrapper(EnvironmentInterface):
 
     
     def _get_embedding_columns(self):
-        return [ FeatureUnit.alliance, \
-                FeatureUnit.health, \
-                FeatureUnit.shield, \
-                FeatureUnit.energy, \
-                FeatureUnit.owner, \
-                FeatureUnit.x, \
-                FeatureUnit.y]
+        return np.array([FeatureUnit.alliance,
+                         FeatureUnit.health,
+                         FeatureUnit.shield,
+                         FeatureUnit.energy,
+                         FeatureUnit.owner,
+                         FeatureUnit.x,
+                         FeatureUnit.y])
 
     def _get_unit_embeddings(self, timestep):
-        unit_info = timestep.observation.feature_units
-        useful_columns = self._get_embedding_columns() 
+        unit_info = np.array(timestep.observation.feature_units)
+        useful_columns = self._get_embedding_columns()
+        adjusted_info = unit_info[:, np.array(useful_columns)]
 
-        adjusted_info = unit_info[:,useful_columns]
-       
-        NUM_UNIT_TYPES = len(static_data.UNIT_TYPES)
-        blizzard_unit_type = unit_info[:,FeatureUnit.unit_type]
+        num_unit_types = len(static_data.UNIT_TYPES)
+        blizzard_unit_type = unit_info[:, FeatureUnit.unit_type]
         pysc2_unit_type = [static_data.UNIT_TYPES.index(t) for t in blizzard_unit_type]
-        one_hot_unit_types = np.eye(NUM_UNIT_TYPES)[pysc2_unit_type]
+        one_hot_unit_types = np.eye(num_unit_types)[pysc2_unit_type]
 
-        unit_vector = np.concatenate([adjusted_info, one_hot_unit_types],axis=-1)
+        unit_vector = np.concatenate([adjusted_info, one_hot_unit_types], axis=-1)
         return unit_vector
 
 

sc2ai/environment.py
@@ -96,10 +96,7 @@ class MultipleEnvironment:
 
     def get_results(self):
         states, masks, rewards, dones = zip(*[pipe.recv() for pipe in self.pipes])
-        try:
-            return states, np.stack(masks), np.stack(rewards), np.stack(dones)
-        except Exception:
-            a = 1 + 1
+        return states, np.stack(masks), np.stack(rewards), np.stack(dones)
 
     def close(self):
         for pipe in self.pipes:

sc2ai/tflearner/tf_agent.py
@@ -233,19 +233,18 @@ class LSTMAgent(InterfaceAgent):
         return tf.squeeze(train_output, axis=0)
 
     def get_feed_dict(self, states, masks, actions=None, bootstrap_state=None):
-
         screens = np.stack([state['screen'] for state in states], axis=0)
-        unit_embeddings = np.stack([state['unit_embeddings'] for state in states], axis=0)
 
         feed_dict = super(LSTMAgent, self).get_feed_dict(screens, masks, actions)
+        all_states = states if bootstrap_state is None else [*states, bootstrap_state]
+        unit_embeddings = util.pad_stack([state['unit_embeddings'] for state in all_states], pad_axis=0, stack_axis=0)
+        feed_dict[self.unit_embeddings_input] = unit_embeddings
+
         if bootstrap_state is not None:
             bootstrap_screen = np.expand_dims(bootstrap_state['screen'], axis=0)
-            bootstrap_units = np.expand_dims(bootstrap_state['unit_embeddings'], axis=0)
             feed_dict[self.state_input] = np.concatenate([screens, bootstrap_screen], axis=0)
-            feed_dict[self.unit_embeddings_input] = np.concatenate([unit_embeddings, bootstrap_units], axis=0)
         else:
             feed_dict[self.state_input] = screens
-            feed_dict[self.unit_embeddings_input] = unit_embeddings
 
         return feed_dict
 