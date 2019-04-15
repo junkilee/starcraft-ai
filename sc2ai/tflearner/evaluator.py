c39e3dcbb1c95e19c28ecd5bd3ab65f26a86ccc9
Your Name <danieldritter1@gmail.com>
2019-04-15 18:02:58 -0400

    Added evaluator.py, which has the MoveToBeacon evaluator function

---
+++ sc2ai/tflearner/evaluator.py
@@ -0,0 +1,29 @@
+import numpy as np
+import math
+class Evaluator:
+
+    def __init__(self):
+        #TODO: Add info from previous timestep
+        pass
+
+    def at_beacon(self,timestep):
+        """
+        Function to check if MoveToBeacon agent
+        has reached the beacon
+        Params: timestep from game
+        Output: True or False
+        """
+        marine = None
+        beacon = None
+        # minimum distance required to return true
+        min_dist = 8
+        for unit in timestep.observation.feature_units:
+            if unit["unit_type"] == 48:
+                marine = unit
+            elif unit["unit_type"] == 317:
+                beacon = unit
+        coordinates = np.asarray([(marine["x"],marine["y"]),(beacon["x"],beacon["y"])])
+        if np.linalg.norm(coordinates[0]-coordinates[1]) < min_dist:
+            return True
+        else:
+            return False
