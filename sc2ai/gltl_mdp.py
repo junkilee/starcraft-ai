0f3f7e0c8daca6e5e1ea5bf72c0261b29e71ca62
Kevin Du <kevin_du@brown.edu>
2019-04-17 12:25:17 -0400

    added acc, rej, init indices to gltlmdp

--- sc2ai/gltl_mdp.py
+++ sc2ai/gltl_mdp.py
@@ -1,7 +1,7 @@
 import numpy as np
 from gltl_tools.gltl.gltl import gltl_to_mdp
 from gltl_tools.gltl.utils import mdp_to_dict_matrix, return_ordered_aps
-
+from sc2ai.tflearner.evaluator import Evaluator
 class GLTLMDP:
     """
     A class representing the mental state of an agent using GLTL. It manages transitions between state based on a list of propositions.
@@ -16,12 +16,15 @@ class GLTLMDP:
         self.currentState = self.mdp.init
         self.transitionMatrices = mdp_to_dict_matrix(self.mdp)
         self.aps = return_ordered_aps(self.mdp)
-
+        self.num_task_states = len(self.mdp["states"])
+        self.acc_state_index = next(iter(self.mdp["accept"]))
+        self.rej_state_index = next(iter(self.mdp["reject"]))
+        self.evaluator = Evaluator()
         # make sure that APs are a subset of proposition dict
         assert(set(self.aps) in propositionDict)
 
     def transition(self, timestep):
-        results_dict = classify(timestep)
+        results_dict = self.evaluator(timestep)
         transitionMatrix = get_next_trans_mat(self.transition_mats_dict, results_dict, self.ordered_aps)
         transitionRow = transitionMatrix[self.currentState]
         self.currentState = np.random.choice(len(transitionRow), 1, p=transitionRow)
