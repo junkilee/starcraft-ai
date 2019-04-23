e80e76331c73ac9aeb791f9d3c98c3ed96b69b23
Jun Ki Lee <joongee@gmail.com>
2019-04-23 16:01:25 -0400

    adding abstractions to actions and observations

--- sc2ai/envs/actions.py
+++ sc2ai/envs/actions.py
@@ -1,22 +1,67 @@
 from pysc2.lib import actions as pysc2_actions
+from abc import ABC, abstractmethod
 
-def convert_to_gym_action_spaces(actionlist):
-    gym_space = None
+class ActionList():
+    """
 
-    for action in  actionlist:
+    """
+    def __init__(self, actionlist):
         pass
 
-    return gym_space
+    def convert_to_gym_action_spaces(actionlist):
+        registered
+
+        for action in actionlist:
+            pass
+
+        gym_space = None
+
+        return gym_space
 
 
-
-class Action:
+class Action(ABC):
     """
+    An abstract class for a default action which relies only on the policy network's output
 
     """
-    def __init__(self):
+    def __init__(self, function_type, **kwargs):
+        self.function_type = function_type
+        self.default_arguments = kwargs
+
+    def __call__(self, action_values):
+
+        return
+
+    @abstractmethod
+    def parameter_type(self):
+        pass
+
+class HighLevelAction(Action):
+    """
+    An abstract class for a high level action which relies on an agent's observation other than the policy network's
+    output itself
+
+    """
+    def __init__(self, function_type, **kwargs):
+        super().__init__(function_type, **kwargs)
+
+    def __call__(self, action_values):
+
+        return
+
+    @abstractmethod
+    def parameter_type(self):
+        pass
+
+
+def SelectAction(Action):
+    def __call__(self):
+        return
+
+    def parameter_type(self):
         return None
 
+def SelectAllAction(Action):
     def __call__(self):
         return
 
@@ -24,41 +69,14 @@ class Action:
         return None
 
 
-def SelectAction():
-    def __init__():
-        super().__init__()
-
+def MoveAction(Action):
     def __call__(self):
         return
 
     def parameter_type(self):
         return None
 
-def SelectAllAction():
-    def __init__():
-        super().__init__()
-
-    def __call__(self):
-        return
-
-    def parameter_type(self):
-        return None
-
-
-def MoveAction():
-    def __init__():
-        super().__init__()
-
-    def __call__(self):
-        return
-
-    def parameter_type(self):
-        return None
-
-def AttackAction():
-    def __init__():
-        super().__init__()
-
+def AttackAction(Action):
     def __call__(self):
         return
 
--- sc2ai/envs/observations.py
+++ sc2ai/envs/observations.py
@@ -1,13 +1,52 @@
 """A set of observation filters which convert observation features from pysc2 to processed numpy arrays
 """
 from pysc2.lib.features import PlayerRelative, FeatureUnit
+from abc import ABC, abstractmethod
 
-
-def convert_to_gym_observation_spaces(filterlist):
-    gym_space = None
-
-    for filter in filterlist:
+class ObservationList():
+    def __init__(self, observationlist):
         pass
 
-    return gym_space
+    def generate_observation(self):
+        """
 
+        Returns:
+
+        """
+        return
+
+    def convert_to_gym_observation_spaces(filterlist):
+        gym_space = None
+
+        for filter in filterlist:
+            pass
+
+        return gym_space
+
+class ObservationFilter(ABC):
+    """
+
+    """
+    def __init__(self, **kwargs):
+        return None
+
+    @abstractmethod
+    def filter(self, observation):
+        """
+
+        Args:
+            observation:
+
+        Returns:
+
+        """
+        pass
+
+
+class PlayerRelativeLocationMap(ObservationFilter):
+    def __init__(self, **kwargs):
+        super().__init()
+
+class SelfUnitLocations(ObservationFilter):
+    def __init__(self, **kwargs):
+        super().__init()
