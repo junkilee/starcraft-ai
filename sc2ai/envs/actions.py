080424eb45930a55473ee89b14deecee7cc80ce1
Jun Ki Lee <joongee@gmail.com>
2019-05-06 13:23:05 -0400

    changing the main structure of action class abstraction

--- sc2ai/envs/actions.py
+++ sc2ai/envs/actions.py
@@ -1,13 +1,12 @@
 from pysc2.lib import actions
+from sc2ai.envs import game_info
 from abc import ABC, abstractmethod
 
-class ActionList():
+class ActionList:
     """Stores a action list for an environment."""
-    def __init__(self, actions_list):
+    def __init__(self, actions_list, reorder_action_id = False):
         self.actions_list = actions_list
-
-        for i, action in enumerate(self.actions_list):
-            action.register_action_id(i)
+        self.reorder_action_id = reorder_action_id
 
         self.num_actions = len(actions_list)
         self.current_num_actions = self.num_actions
@@ -18,7 +17,15 @@ class ActionList():
 
     def update_available_actions(self, available_actions):
 
+    def transform_action(self, observation, action_values):
+        action_id = action_values[0]
 
+        if self.reorder_action_id:
+            raise NotImplementedError
+        else:
+            actions = self.actions_list[action_id].transform_action(observation, action_values[1:])
+
+        return actions
 
     def convert_to_gym_action_spaces(self, actionlist):
         vectors = []
@@ -26,26 +33,71 @@ class ActionList():
         vectors.add(self.num_actions)
 
         for action in actionlist:
-            pass
+            vectors.append(action.retrieve_parameter_size_vectors())
 
         gym_space = None
 
         return gym_space
 
 
-class Action(ABC):
+class Action:
     """An abstract class for a default action which relies only on the policy network's output"""
-    def __init__(self, function_type, arg_types, **kwargs):
-        self.function_type = function_type
+    def __init__(self, arg_types, **kwargs):
         self.default_arguments = kwargs
         self.arg_types = arg_types
         self.action_id = None
+        self.feature_screen_size = kwargs['feature_screen_size'] if 'feature_screen_size' in kwargs else None
+        self.feature_minimap_size = kwargs['feature_minimap_size'] if 'feature_minimap_size' in kwargs else None
+
+    def retrieve_parameter_size_vectors(self):
+        vectors = []
+        for arg_type in self.arg_types:
+            if arg_type.sizes == (0,0):
+                if arg_type is actions.TYPES.screen:
+                    feature_screen_size = self.feature_screen_size or game_info.feature_screen_size
+                    vectors.add(feature_screen_size**2)
+                elif arg_type is actions.TYPES.minimap:
+                    feature_minimap_size = self.feature_minimap_size or game_info.feature_minimap_size
+                    vectors.add(feature_minimap_size**2)
+                else:
+                    raise NotImplementedError
+            else:
+                vectors.add(arg_type.sizes)
+        return vectors
 
 
+class AtomAction(Action):
+    """A Class made to directly mirror pysc2 523 default actions"""
+    def __init__(self, function_tuple, **kwargs):
+        self.function_tuple= function_tuple
+        self.function_type = function_tuple.function_type
+        arg_types = function_tuple.args
+        super().__init__(self, arg_types, **kwargs)
 
-    @abstractmethod
+
+class SelectPointAction(AtomAction):
+    def __init__(self, **kwargs):
+        super().__init__(actions.FUNCTIONS.select_point, **kwargs)
+
+class SelectPointAction(AtomAction):
+    def __init__(self, **kwargs):
+        super().__init__(actions.FUNCTIONS.select_point, **kwargs)
+
+def MoveAction(Action):
+    def __call__(self):
+        return
+
     def parameter_type(self):
-        pass
+        return None
+
+
+def AttackAction(Action):
+    def __call__(self):
+        return
+
+    def parameter_type(self):
+        return None
+
 
 class HighLevelAction(Action):
     """
@@ -60,42 +112,4 @@ class HighLevelAction(Action):
 
         return
 
-    @abstractmethod
-    def parameter_type(self):
-        pass
-
-
-def SelectAction(Action):
-    def __init__(self, function_type, **kwargs):
-        super().__init__(function_type, **kwargs)
-        self.arg_types = []
-
-    def __call__(self):
-        return
-
-    def parameter_type(self):
-        return None
-
-def SelectAllAction(Action):
-    def __call__(self):
-        return
-
-    def parameter_type(self):
-        return None
-
-
-def MoveAction(Action):
-    def __call__(self):
-        return
-
-    def parameter_type(self):
-        return None
-
-def AttackAction(Action):
-    def __call__(self):
-        return
-
-    def parameter_type(self):
-        return None
-
-
+if __name__ == '__main__':
